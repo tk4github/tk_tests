6154eedbc2a84d0e4adf14a347c125c49d202e0f
thke <tkemper@sii.fr>
2015-10-06 15:53:26 +0200

    update2

--- tk_HelloWorld.java
+++ tk_HelloWorld.java
@@ -5,5 +5,6 @@ public class tk_HelloWorld
 		System.out.println("Hello TK");
 		System.out.println("This seems to work nicely");
 		System.out.println("update 1");
+		System.out.println("update 2");
 	}
 }
