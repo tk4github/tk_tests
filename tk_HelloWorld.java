c9a23480c495a7daf3890a48e8e3366a24f36017
Thibaut Kemper <tkemper@tk.vm.vdi.s1.p.fti.net>
2015-11-25 16:35:18 +0100

    et voila une ligne ajoutee pour tester l'autobuild

--- tk_HelloWorld.java
+++ tk_HelloWorld.java
@@ -4,7 +4,6 @@ public class tk_HelloWorld
 	{
 		System.out.println("Hello TK");
 		System.out.println("This seems to work nicely");
-		System.out.println("update 1");
-		System.out.println("update 2");
+		System.out.println("update to test the autobuild");
 	}
 }
